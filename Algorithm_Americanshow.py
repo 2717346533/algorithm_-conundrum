de42f3f357fe6d13d0acb6ea9c2e64a5d18d1d33
九牛一牧 <34566246+2717346533@users.noreply.github.com>
2019-09-25 20:24:14 +0800

    算法谜题9

--- Algorithm_Americanshow.py
+++ Algorithm_Americanshow.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python
 # 每次生成并测试一个组合
-def HireShow(candList,candTalents,talentList):
+def Hire4Show(candList,candTalents,talentList):
     n = len(candList)
     hire = candList[:]
     for i in range(2**n):
@@ -7,7 +8,7 @@ def HireShow(candList,candTalents,talentList):
         num = i
         for j in range(n):
             if(num % 2 == 1):
-                Combination = [candList[n-1-j] + Combination]
+                Combination = [candList[n-1-j]] + Combination
             num = num // 2
         if Good(Combination,candList,candTalents,talentList):
             if len(hire) > len(Combination):
@@ -19,15 +20,15 @@ def Good(Comb,candList,candTalents,AllTalents):
     for tal in AllTalents:
         cover = False
         for cand in Comb:
-            candTal = candTalents[candList.index(card)]
+            candTal = candTalents[candList.index(cand)]
             if tal in candTal:
                 cover = True
         if not cover:
-            return Flase
+            return False
     return True
 
 Talents = ['Sing','Dance','Magic','Act','Flex','Code']
 Candidates = ['Aly','Bob','Cal','Don','Eve','Fay']
-CandidateTalents = [['Flex','Code'],['Dance','Magic'],['Sing','Magic'],['Dance','Act','Code'],['Act','Code']]
+CandidateTalents = [['Flex','Code'],['Dance','Magic'],['Sing','Magic'],['Sing','Dance'],['Dance','Act','Code'],['Act','Code']]
 
 Hire4Show(Candidates,CandidateTalents,Talents)
